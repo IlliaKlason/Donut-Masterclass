5be5204923f8db1be50b10903569389dfd1efe49
IlliaKlason <105161428+IlliaKlason@users.noreply.github.com>
2022-07-11 18:30:01 +0000

    Deploying to gh-pages from @ IlliaKlason/angrybirds@559fa8d300b005f86536334164c263936bbb084c 🚀

--- index.a27d2dcc.js
+++
@@ -1,2 +0,0 @@
-$(".reviews__list").slick({dots:!0,arrows:!1,infinite:!1,speed:300,slidesToShow:3,slidesToScroll:1,responsive:[{breakpoint:1024,settings:{slidesToShow:3,slidesToScroll:3,infinite:!0}},{breakpoint:600,settings:{slidesToShow:2,slidesToScroll:2}},{breakpoint:480,settings:{slidesToShow:1,slidesToScroll:1}}]});
-//# sourceMappingURL=index.a27d2dcc.js.map
---
+++ index.b5c1aac8.js
@@ -0,0 +1,2 @@
+$(".reviews__list").slick({autoplay:!1,centerMode:!0,infinite:!0,arrows:!1,slidesToShow:3,slidesScroll:1,variableWidth:!0,dots:!1,responsive:[{breakpoint:768,settings:{variableWidth:!0,slidesToShow:3,slidesScroll:1,centerMode:!0,arrows:!1}},{breakpoint:320,settings:{variableWidth:!0,slidesToShow:1,slidesScroll:1,centerMode:!0,arrows:!1}}]});
+//# sourceMappingURL=index.b5c1aac8.js.map
