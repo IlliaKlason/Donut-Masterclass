6e2f55d8b0028caa60ca29c27ec55ed336d6fa14
IlliaKlason <105161428+IlliaKlason@users.noreply.github.com>
2022-07-11 13:17:11 +0000

    Deploying to gh-pages from @ IlliaKlason/angrybirds@156b2d976e62b32946e15d580118a0d519afe0a1 🚀

---
+++ index.54b386e6.js
@@ -0,0 +1,2 @@
+$(".reviews__list").slick({dots:!0,arrows:!1,infinite:!1,speed:300,slidesToShow:3,slidesToScroll:1,responsive:[{breakpoint:1024,settings:{slidesToShow:3,slidesToScroll:3,infinite:!0}},{breakpoint:600,settings:{slidesToShow:2,slidesToScroll:2}},{breakpoint:480,settings:{slidesToShow:1,slidesToScroll:1}}]});
+//# sourceMappingURL=index.54b386e6.js.map
