f5ce524567376bfd33ca5a01f27147ce5d9bcd3d
IlliaKlason <105161428+IlliaKlason@users.noreply.github.com>
2022-07-12 14:20:26 +0300

    Fix JS

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,26 +3,19 @@
   const mobileMenu = document.querySelector('.m-burger');
   const openMenuBtn = document.querySelector('.menu-burger-btn');
   const body = document.querySelector('body');
-
-
-
+  const mobileLinkRef = document.querySelector('#menu');
 
   const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
     openMenuBtn.classList.toggle('is-open');
     body.classList.toggle('no-scroll');
-
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-
-
+    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
+  mobileLinkRef.addEventListener('click', toggleMenu);
 
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
@@ -33,6 +26,10 @@
     openMenuBtn.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
   });
+  mobileLinkRef.addEventListener("click", () => {
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.classList.remove('is-open');
+    body.classList.remove('no-scroll');
+    openMenuBtn.setAttribute('aria-expanded', false)
+  });
 })();
-
-
--- src/js/slick.js
+++ src/js/slick.js
@@ -1,38 +1,47 @@
-
 $('.reviews__list').slick({
-    autoplay: false,
-    centerMode: true,
-    infinite: true,
-    arrows: false,
-    slidesToShow: 3,
-    slidesScroll: 1,
-    variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    speed: 300,
-    cssEase: 'linear',
-    dots: false,
-    responsive: [{
-        breakpoint: 768,
-        settings: {
-            variableWidth: true,
-            slidesToShow: 3,
-            slidesScroll: 1,
-            centerMode: true,
-            arrows: false
-        }
+  autoplay: true,
+  autoplaySpeed: 1500,
+  speed: 300,
+  easing: 'ease',
+  centerMode: true,
+  centerPadding: '0px',
+  adaptiveHeight: true,
+  infinite: true,
+  arrows: false,
+  slidesToShow: 3,
+  slidesScroll: 1,
+  variableWidth: true,
+  dots: false,
+  asNavFor: '.reviews__description',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        variableWidth: true,
+        slidesToShow: 3,
+        slidesScroll: 1,
+        centerMode: true,
+        arrows: false,
+      },
     },
     {
-        breakpoint: 320,
-        settings: {
-            variableWidth: true,
-            slidesToShow: 1,
-            slidesScroll: 1,
-            centerMode: true,
-            arrows: false
-        }
-    }
-        // instead of a settings object
-    ]
+      breakpoint: 320,
+      settings: {
+        variableWidth: true,
+        slidesToShow: 1,
+        slidesScroll: 1,
+        centerMode: true,
+        arrows: false,
+      },
+    },
+  ],
 });
-
+$('.reviews__description').slick({
+  asNavFor: '.reviews__list',
+  dots: true,
+  arrows: false,
+  adaptiveHeight: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesScroll: 1,
+});
