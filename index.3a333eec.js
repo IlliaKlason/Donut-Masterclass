7d358ba3a4b029909eba8b867027541b2c18a87e
IlliaKlason <105161428+IlliaKlason@users.noreply.github.com>
2022-07-13 20:19:04 +0000

    Deploying to gh-pages from @ IlliaKlason/angrybirds@d6db3b1e0c940351ce5e9d458991aae33dd3451b 🚀

--- index.3a333eec.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".btn-up").addEventListener("click",(()=>{window.scrollTo(0,0)}));const n=document.querySelector(".btn-up");window.onscroll=()=>{window.scrollY>700?n.classList.remove("btn-hidden"):window.scrollY<700&&n.classList.add("btn-hidden")};
-//# sourceMappingURL=index.3a333eec.js.map
--- index.e9eb802d.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector(".btn-up").addEventListener("click",(function(){window.scrollTo(0,0)}));var n=document.querySelector(".btn-up");window.onscroll=function(){window.scrollY>700?n.classList.remove("btn-hidden"):window.scrollY<700&&n.classList.add("btn-hidden")}}();
-//# sourceMappingURL=index.e9eb802d.js.map
